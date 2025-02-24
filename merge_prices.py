077c2e5b271d66f625b84e07fb75f38316e42ea1
Peng Ding <pding.dp@foxmail.com>
2025-02-24 15:22:36 -0600

    separate load balance prices. clean duplicated script

--- merge_prices.py
+++ merge_prices.py
@@ -155,11 +155,16 @@ def integrate_prices(primary_prices, secondary_prices):
 
 
 if __name__ == "__main__":
-    # 指定YAML文件的路径
+    # 主要手工定价表格
     yaml_file_path = "manual_prices.yaml"
-    # 调用函数将YAML转换为JSON
     manual_prices = yaml_to_json(yaml_file_path)
-
+    # 负载均衡定价表 —— 主要用于deepseek和bge
+    yaml_file_path_load_balance = "manual_prices_loadbalance.yaml"
+    manual_prices_loadbalance = yaml_to_json(yaml_file_path_load_balance)
+    # 集成手动价格和 siliconflow_prices
+    integrated_manual_prices = integrate_prices(
+        manual_prices, manual_prices_loadbalance
+    )
     # 读取 siliconflow_prices.json 文件
     try:
         with open("siliconflow_prices.json", "r", encoding="utf-8") as file:
@@ -169,42 +174,7 @@ if __name__ == "__main__":
         siliconflow_prices = {"data": []}
 
     # 集成手动价格和 siliconflow_prices
-    integrated_prices = integrate_prices(manual_prices, siliconflow_prices)
-
-    # 获取 provider 的价格
-    try:
-        response = requests.get(
-            "https://raw.githubusercontent.com/MartialBE/one-api/prices/prices.json"
-        )
-        response.raise_for_status()
-        provider_prices = {"data": response.json()}
-    except requests.RequestException as e:
-        print(f"获取 provider 价格出错: {e}")
-        provider_prices = {"data": []}
-
-    # 集成 provider 的价格，确保手动价格优先
-    final_prices = integrate_prices(integrated_prices, provider_prices)
-
-    # 将集成后的价格数据保存到 oneapi_prices.json 文件
-    with open("oneapi_prices.json", "w", encoding="utf-8") as file:
-        json.dump(final_prices, file, indent=2, ensure_ascii=False)
-
-    print("已将集成后的价格数据保存到 oneapi_prices.json 文件。")
-    # 指定YAML文件的路径
-    yaml_file_path = "manual_prices.yaml"
-    # 调用函数将YAML转换为JSON
-    manual_prices = yaml_to_json(yaml_file_path)
-
-    # 读取 siliconflow_prices.json 文件
-    try:
-        with open("siliconflow_prices.json", "r", encoding="utf-8") as file:
-            siliconflow_prices = json.load(file)
-    except FileNotFoundError:
-        print("未找到 siliconflow_prices.json 文件，将使用手动价格作为最终结果。")
-        siliconflow_prices = {"data": []}
-
-    # 集成手动价格和 siliconflow_prices
-    integrated_prices = integrate_prices(manual_prices, siliconflow_prices)
+    integrated_prices = integrate_prices(integrated_manual_prices, siliconflow_prices)
 
     # 获取 provider 的价格
     try:
